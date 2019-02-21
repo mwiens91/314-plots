f1e2bf70a3f3536b95adb9bb08293643a32c462b
Matt Wiens <mwiens91@gmail.com>
2019-02-20 18:38:00 -0800

    Add 6.2.2 code + figure

--- ps3/ps3_a.m
+++ ps3/ps3_a.m
@@ -1,9 +1,10 @@
 % Set values for plotting
-xs = linspace(0, 1, 150);
+num_xs = 150;
+xs = linspace(0, 1, num_xs);
 
 %% 6.1.4
 
-% Fourier series terms
+% Fourier sine series terms
 lambda = @(n) n^2 * pi^2;
 n_term = @(x, t, n) ...
   2 * (1 - (-1)^n) * ...
@@ -36,7 +37,7 @@ title('6.1.4');
 
 %% 6.1.7
 
-% Fourier series terms
+% Fourier sine series terms
 lambda = @(n) n^2 * pi^2;
 n_term = @(x, t, n) ...
   2*(-1)^n/(n*pi*(lambda(n)^2 + 1)) * ...
@@ -68,3 +69,41 @@ plot(xs, fs(1, :), 'k', xs, fs(2, :), 'k--', xs, fs(3, :), 'k:', xs, fs(4, :), '
 
 legend(strcat('u(x, ', string(ts), ')'));
 title('6.1.7');
+
+%% 6.2.2
+
+% Fourier cosine series terms (n > 0)
+lambda = @(n) n^2 * pi^2;
+n_term = @(x, t, n) ...
+  2/lambda(n)*( ...
+    (1-(-1)^n)*(1-exp(-lambda(n)*t))/lambda(n) - ...
+    (1+(-1)^n)*exp(-lambda(n)*t) ...
+  ) * ...
+  cos(n*pi*x);
+a0 = 1/6;
+a0_vec = a0 * ones(1, num_xs);
+
+% Fourier series values for different t
+ts = [0, 0.02, 0.04, 0.06];
+fs = zeros(length(ts), length(xs));
+row = 1;
+
+for t = ts
+  f = 0;
+
+  for n = 1:2
+    f = f + n_term(xs, t, n);
+  end
+
+  f = f + a0;
+
+  fs(row, :) = f;
+  row = row + 1;
+end
+
+% Plotting the computed series
+figure();
+plot(xs, fs(1, :), 'k.', xs, fs(2, :), 'k--', xs, fs(3, :), 'k:', xs, fs(4, :), 'k-.', xs, a0_vec, 'k-');
+
+legend([strcat('u(x, ', string(ts), ')'), 'steady state']);
+title('6.2.2');
